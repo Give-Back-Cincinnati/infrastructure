a725c5b5dd40be637dde87bf9021987847fd6f30
Michael Young <michael25young25@gmail.com>
2022-12-01 22:47:03 -0500

    add an r2 bucket

--- cloudflare.tf
+++ cloudflare.tf
@@ -6,10 +6,28 @@ variable "cloudflare_api_key" {
   type = string
 }
 
+variable "r2_access_key" {
+    type = string
+}
+
+variable "r2_secret_key" {
+    type = string
+}
+
 provider "cloudflare" {
   email   = var.cloudflare_email
   api_key = var.cloudflare_api_key
+}
 
+provider "aws" {
+  access_key = var.r2_access_key
+  secret_key = var.r2_secret_key
+  skip_credentials_validation = true
+  skip_region_validation = true
+  skip_requesting_account_id = true
+  endpoints {
+    s3 = "https://352594426cd9ff5b7c6785315b51a93d.r2.cloudflarestorage.com"
+  }
 }
 
 locals {
@@ -51,6 +69,10 @@ resource "cloudflare_record" "gbc_next" {
   type    = "CNAME"
   proxied = true
   ttl     = 1
+}
+
+resource "aws_s3_bucket" "cloudflare-bucket" {
+  bucket = "gbc-static"
 }
 
 resource "cloudflare_zone_settings_override" "gbc_dev_settings" {
