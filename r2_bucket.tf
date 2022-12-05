dbf05fb936ff24f4b55f25ef93533f7d0a6c03a7
Michael Young <michael25young25@gmail.com>
2022-12-05 00:16:10 -0500

    remove malformed cors rule

--- r2_bucket.tf
+++ r2_bucket.tf
@@ -41,11 +41,6 @@ resource "aws_s3_bucket_cors_configuration" "example" {
 
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["GET"]
-    allowed_origins = [""]
-  }
-  cors_rule {
-    allowed_headers = ["*"]
     allowed_methods = ["PUT"]
     allowed_origins = ["https://${cloudflare_record.gbc_next.name}"]
   }
